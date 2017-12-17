fc437ebd90b8d83afd2af715c42880de0bc99c32
Lillian Chernin <l.smith1654@gmail.com>
2017-12-16 19:43:01 -0600

    start formatting html page

---
+++ app.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+
+// const dataArray = [];
+//
+// const retrieveVerseNumber = function(currentText) {
+//   let curTexArr = currentText.split('');
+//   for (let i = 0; i < curTexArr.length; i++) {
+//     if ((curTexArr[i].charCodeAt(0) >= 65 && curTexArr[i].charCodeAt(0) <= 90) || (curTexArr[i].charCodeAt(0) >= 97 && curTexArr[i].charCodeAt(0) <= 122)) {
+//       return curTexArr.slice(0, i).join('');
+//     }
+//   }
+// }
+//
+// const retrieveVerseText = function(currentText) {
+//   let curTexArr = currentText.split('');
+//   for (let i = 0; i < curTexArr.length; i++) {
+//     if ((curTexArr[i].charCodeAt(0) >= 65 && curTexArr[i].charCodeAt(0) <= 90) || (curTexArr[i].charCodeAt(0) >= 97 && curTexArr[i].charCodeAt(0) <= 122)) {
+//       return curTexArr.slice(i).join('');
+//     }
+//   }
+// }
+//
+//
+// $('.verse').each(function(i, obj) {
+//   let currentObj = {};
+//   let currentText = $(this).text();
+//   currentObj.verseText = retrieveVerseText(currentText);
+//   currentObj.verseNumber = retrieveVerseNumber(currentText);
+//   currentObj.bookName = "Genesis";
+//   currentObj.chapterNumber = 3;
+//   currentObj.section = "Old Testament";
+//   dataArray.push(currentObj);
+// });
+//
+//
+// JSON.stringify(dataArray);
+
+
+
+// var classList = $(this).attr('class').split(/\s+/);
+// $.each(classList, function(index, item) {
+//     console.log(item + index);
+//     if (item !== 'verse') {
+//       console.log(item);
+//       let verseNum = Number(item.split('').shift().join(''));
+//       return currentObj.verseNumber = verseNum;
+//     }
+// });
---
+++ parser.js
@@ -0,0 +1,35 @@
+
+const dataArray = [];
+
+const retrieveVerseNumber = function(currentText) {
+  let curTexArr = currentText.split('');
+  for (let i = 0; i < curTexArr.length; i++) {
+    if ((curTexArr[i].charCodeAt(0) >= 65 && curTexArr[i].charCodeAt(0) <= 90) || (curTexArr[i].charCodeAt(0) >= 97 && curTexArr[i].charCodeAt(0) <= 122)) {
+      return curTexArr.slice(0, i).join('');
+    }
+  }
+}
+
+const retrieveVerseText = function(currentText) {
+  let curTexArr = currentText.split('');
+  for (let i = 0; i < curTexArr.length; i++) {
+    if ((curTexArr[i].charCodeAt(0) >= 65 && curTexArr[i].charCodeAt(0) <= 90) || (curTexArr[i].charCodeAt(0) >= 97 && curTexArr[i].charCodeAt(0) <= 122)) {
+      return curTexArr.slice(i).join('');
+    }
+  }
+}
+
+
+$('.verse').each(function(i, obj) {
+  let currentObj = {};
+  let currentText = $(this).text();
+  currentObj.verseText = retrieveVerseText(currentText);
+  currentObj.verseNumber = retrieveVerseNumber(currentText);
+  currentObj.bookName = "Genesis";
+  currentObj.chapterNumber = 3;
+  currentObj.section = "Old Testament";
+  dataArray.push(currentObj);
+});
+
+
+JSON.stringify(dataArray);
