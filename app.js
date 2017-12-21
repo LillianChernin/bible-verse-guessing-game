73d52fd76c251bc4d721847a5427d84e069c645e
Lillian Chernin <l.smith1654@gmail.com>
2017-12-21 14:26:55 -0600

    add the book of leviticus

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let userBookChoice = "";
 let userChapterChoice = "";
 let userVerseChoice = "";
 
-const books = ["genesis", "matthew", "mark", "luke", "john"];
+const books = ["genesis", "exodus", "leviticus","matthew", "mark", "luke", "john"];
 
 const randomBookSelector = () => {
   let randomNum = Math.floor(Math.random() * books.length);
@@ -36,6 +36,9 @@ const randomVerseGenerator = () => {
   } else if (randomBook === "exodus") {
     lastChapter = exodusVerses[exodusVerses.length - 1].chapterNumber;
     return randomVerseSelector(exodusVerses);
+  } else if (randomBook === "leviticus") {
+    lastChapter = leviticusVerses[leviticusVerses.length - 1].chapterNumber;
+    return randomVerseSelector(leviticusVerses);
   } else if (randomBook === "matthew") {
     lastChapter = matthewVerses[matthewVerses.length - 1].chapterNumber;
     return randomVerseSelector(matthewVerses);
